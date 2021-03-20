f3904d7618ed18cfbedb200f9046dd46bd33dbed
Hugues <hugues.aubry@gmail.com>
2021-03-20 19:24:21 +0100

    [DEV] build lightbox & counterLike
    - struture HTML/CSS
    - JS open & close lightbox
    
    - counterLike html/css build
    
    - work on photographer cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ====== open and close form ======
 const bgModal = document.getElementsByClassName("bground")[0];
 const btnModal = document.getElementsByClassName("ph-contact btn")[0];
 const closeModal = document.getElementById("cross-close");
@@ -16,5 +17,27 @@ closeModal.onclick = function () {
 window.onclick = function (close) {
   if (close.target == bgModal) {
     bgModal.style.display = "none";
+  }
+};
+
+// ====== open & close lightbox
+const bgLightbox = document.getElementsByClassName("bground-lightbox")[0];
+const openLightbox = document.getElementById("openLightbox");
+const closeLightbox = document.getElementById("lightbox-cross-close");
+
+// When the user clicks the button, open the modal
+openLightbox.onclick = function () {
+  bgLightbox.style.display = "block";
+};
+
+// When the user clicks on <svg> (x), close the modal
+closeLightbox.onclick = function () {
+  bgLightbox.style.display = "none";
+};
+
+// when the user clicks everywhere out of the modal
+window.onclick = function (close) {
+  if (close.target == bgLightbox) {
+    bgLightbox.style.display = "none";
   }
 };
