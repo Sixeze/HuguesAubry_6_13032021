0186d5ff03baa8a485fe64a1e3cb47b8bc4c23d1
Hugues <hugues.aubry@gmail.com>
2021-04-06 18:18:50 +0200

    [DEV]
    - création du fichier json, fetch de ce dernier dans le js
    - re build des pages pour que ce soit plus propre et qu'il y ait moins de répétitions.

--- js/homepage.js
+++ js/homepage.js
@@ -1,272 +1,46 @@
-/** anotation de la page
- * les fonctions retrouvé sur cette page
- * 1. Afficher la liste des Photographes
- * 2. Filtrer par tags
- */
+// fetch json file
+fetch("./js/data.json")
+  .then((response) => response.json())
+  .then((phData) => console.table(phData.photographers));
 
-// import { data } from "./photographData";
-
-// new article + element
+// Javascript index of photograph
 const mainSection = document.getElementsByTagName("main");
 
 function creatArticleCard() {
-  let i;
-  for (i = 0; i < data.photographers.length; i += 1) {
-    const newArticle = document.createElement("article");
-    mainSection[0].appendChild(newArticle);
-    newArticle.setAttribute("class", "photograph__card");
+  fetch("./js/data.json")
+    .then((response) => response.json())
+    .then((phData) => {
+      let i;
+      for (i = 0; i < phData.photographers.length; i += 1) {
+        const newArticle = document.createElement("article");
+        mainSection[0].appendChild(newArticle);
 
-    const aLinkElt = document.createElement("a");
-    newArticle.appendChild(aLinkElt);
-    aLinkElt.setAttribute("href", "./photographerPage.html");
-    aLinkElt.setAttribute("class", "photograph__card--link");
+        newArticle.setAttribute("class", "photograph__card");
+        newArticle.setAttribute("id", "photographer");
+        newArticle.innerHTML = `<a href="./photographerPage.html?dataph=${phData.photographers[i].id}"
+                                    class="photograph__card--link">
+                                    <img src="media/PhotographersIDPhotos/${phData.photographers[i].portrait}"
+                                    alt="Portrait représentant:  ${phData.photographers[i].name}"
+                                    class="artist-pict">
+                                    <h2>${phData.photographers[i].name}</h2></a>
+                                    <aside class="photograph__card--txt">
+                                    <strong>${phData.photographers[i].city}/${phData.photographers[i].country}</strong>
+                                    <p>${phData.photographers[i].tagline}</p>
+                                    <span>${phData.photographers[i].price}€/jour</span>
+                                    <ul class="tagBox"></ul></aside>`;
 
-    const imgElt = document.createElement("img");
-    aLinkElt.appendChild(imgElt);
-    imgElt.setAttribute("class", "artist-pict");
-    imgElt.setAttribute("src", data.photographers[i].portrait);
-    imgElt.setAttribute(
-      "alt",
-      "Portrait représentant: " + data.photographers[i].name
-    );
-
-    const titleNameElt = document.createElement("h2");
-    aLinkElt.appendChild(titleNameElt);
-    titleNameElt.textContent = data.photographers[i].name;
-
-    const asideElt = document.createElement("aside");
-    newArticle.appendChild(asideElt);
-    asideElt.setAttribute("class", "photograph__card--txt");
-
-    const strongElt = document.createElement("strong");
-    asideElt.appendChild(strongElt);
-    strongElt.textContent =
-      data.photographers[i].city + "/" + data.photographers[i].country;
-
-    const pElt = document.createElement("p");
-    asideElt.appendChild(pElt);
-    pElt.textContent = data.photographers[i].tagline;
-
-    const spanElt = document.createElement("span");
-    asideElt.appendChild(spanElt);
-    spanElt.textContent = data.photographers[i].price + "€/jour";
-
-    const ulTag = document.createElement("ul");
-    asideElt.appendChild(ulTag);
-    ulTag.setAttribute("class", "tagBox");
-    const tagsData = data.photographers[i].tags;
-    for (let t = 0; t < tagsData.length; t += 1) {
-      const liTagElt = document.createElement("li");
-      ulTag.appendChild(liTagElt);
-      liTagElt.setAttribute("class", "link-tag");
-      const aTagElt = document.createElement("a");
-      liTagElt.appendChild(aTagElt);
-      aTagElt.setAttribute("href", "#");
-      aTagElt.setAttribute("title", tagsData[t]);
-      aTagElt.textContent = "#" + tagsData[t];
-    }
-  }
+        const tagsData = phData.photographers[i].tags;
+        for (let t = 0; t < tagsData.length; t += 1) {
+          const ulTagBox = document.getElementsByClassName("tagBox");
+          const liTagElt = document.createElement("li");
+          ulTagBox[i].appendChild(liTagElt);
+          liTagElt.setAttribute("class", "link-tag");
+          liTagElt.innerHTML = `<a href="#" tittle="${tagsData[t]}" >#${tagsData[t]}</a> `;
+        }
+      }
+    });
 }
 
 window.onload = () => {
   creatArticleCard();
 };
-
-// function newArticleElt(
-//   articleClassData,
-//   aHrefData,
-//   aClassData,
-//   imgClassData,
-//   imgSrcData,
-//   imgAltData,
-//   artistName,
-//   asideClassData,
-//   cityAndCountry,
-//   tagline,
-//   price
-// ) {
-//   const newArticle = document.createElement("article");
-//   const main = document.getElementsByTagName("main");
-//   const aLinkElt = document.createElement("a");
-//   const imgElt = document.createElement("img");
-//   const titleNameElt = document.createElement("h2");
-//   const asideElt = document.createElement("aside");
-//   const strongElt = document.createElement("strong");
-//   const pElt = document.createElement("p");
-//   const spanElt = document.createElement("span");
-
-//   main[0].appendChild(newArticle);
-
-//   newArticle.appendChild(aLinkElt);
-//   newArticle.appendChild(asideElt);
-
-//   aLinkElt.appendChild(imgElt);
-//   aLinkElt.appendChild(titleNameElt);
-
-//   asideElt.appendChild(strongElt);
-//   asideElt.appendChild(pElt);
-//   asideElt.appendChild(spanElt);
-
-//   newArticle.setAttribute("class", articleClassData);
-
-//   aLinkElt.setAttribute("href", aHrefData);
-//   aLinkElt.setAttribute("class", aClassData);
-
-//   imgElt.setAttribute("class", imgClassData);
-//   imgElt.setAttribute("src", imgSrcData);
-//   imgElt.setAttribute("alt", imgAltData);
-
-//   titleNameElt.textContent = artistName;
-
-//   asideElt.setAttribute("class", asideClassData);
-
-//   strongElt.textContent = cityAndCountry;
-//   pElt.textContent = tagline;
-//   spanElt.textContent = price;
-// }
-
-// for (let i = 0; i < data.photographers.length; i += 1) {
-//   const ulTag = document.createElement("ul");
-//   ulTag.setAttribute("class", "tagBox");
-//   console.log(ulTag);
-//   const tagsData = data.photographers[i].tags;
-//   for (let t = 0; t < tagsData.length; t += 1) {
-//     const liTagElt = document.createElement("li");
-//     ulTag.appendChild(liTagElt);
-//     liTagElt.setAttribute("class", "link-tag");
-//     const aTagElt = document.createElement("a");
-//     liTagElt.appendChild(aTagElt);
-//     aTagElt.setAttribute("href", "#");
-//     aTagElt.setAttribute("title", tagsData[t]);
-//     aTagElt.textContent = "#" + tagsData[t];
-
-//     console.log(liTagElt);
-//     console.log(aTagElt);
-//   }
-// }
-
-// const articlePh1 = newArticleElt(
-//   "photograph__card",
-//   "./photographerPage.html",
-//   "photograph__card--link",
-//   "artist-pict",
-//   data.photographers[0].portrait,
-//   "Portrait représentant :" + data.photographers[0].name,
-//   data.photographers[0].name,
-//   "photograph__card--txt",
-//   data.photographers[0].city + "/" + data.photographers[0].country,
-//   data.photographers[0].tagline,
-//   data.photographers[0].price + "€/jour"
-// );
-// const articlePh2 = newArticleElt(
-//   "photograph__card",
-//   "./photographerPage.html",
-//   "photograph__card--link",
-//   "artist-pict",
-//   data.photographers[1].portrait,
-//   "Portrait représentant :" + data.photographers[1].name,
-//   data.photographers[1].name,
-//   "photograph__card--txt",
-//   data.photographers[1].city + "/" + data.photographers[1].country,
-//   data.photographers[1].tagline,
-//   data.photographers[1].price + "€/jour"
-// );
-// const articlePh3 = newArticleElt(
-//   "photograph__card",
-//   "./photographerPage.html",
-//   "photograph__card--link",
-//   "artist-pict",
-//   data.photographers[2].portrait,
-//   "Portrait représentant :" + data.photographers[2].name,
-//   data.photographers[2].name,
-//   "photograph__card--txt",
-//   data.photographers[2].city + "/" + data.photographers[2].country,
-//   data.photographers[2].tagline,
-//   data.photographers[2].price + "€/jour"
-// );
-// const articlePh4 = newArticleElt(
-//   "photograph__card",
-//   "./photographerPage.html",
-//   "photograph__card--link",
-//   "artist-pict",
-//   data.photographers[3].portrait,
-//   "Portrait représentant :" + data.photographers[3].name,
-//   data.photographers[3].name,
-//   "photograph__card--txt",
-//   data.photographers[3].city + "/" + data.photographers[3].country,
-//   data.photographers[3].tagline,
-//   data.photographers[3].price + "€/jour"
-// );
-// const articlePh5 = newArticleElt(
-//   "photograph__card",
-//   "./photographerPage.html",
-//   "photograph__card--link",
-//   "artist-pict",
-//   data.photographers[4].portrait,
-//   "Portrait représentant :" + data.photographers[4].name,
-//   data.photographers[4].name,
-//   "photograph__card--txt",
-//   data.photographers[4].city + "/" + data.photographers[4].country,
-//   data.photographers[4].tagline,
-//   data.photographers[4].price + "€/jour"
-// );
-// const articlePh6 = newArticleElt(
-//   "photograph__card",
-//   "./photographerPage.html",
-//   "photograph__card--link",
-//   "artist-pict",
-//   data.photographers[5].portrait,
-//   "Portrait représentant :" + data.photographers[5].name,
-//   data.photographers[5].name,
-//   "photograph__card--txt",
-//   data.photographers[5].city + "/" + data.photographers[5].country,
-//   data.photographers[5].tagline,
-//   data.photographers[5].price + "€/jour"
-// );
-
-// function openProfilPage() {
-//   window.location = "photographerPage.html";
-// }
-
-// aLinkElt.addEventListener("click", openProfilPage);
-
-// console.log(aLinkElt);
-
-// const newNamePhotograph = document.createElement("h2");
-
-// const photographCard = document.querySelectorAll(
-//   "a[class=photograph__card--link]"
-// );
-// const firstPH = data.photographers[0];
-// photographCard[0].appendChild(newNamePhotograph);
-// newNamePhotograph.textContent = firstPH.name;
-
-// // creat div with img
-// function creatDivElt(linkClass, stringAL, profilPicture) {
-//   let divElt = document.createElement("div");
-//   divElt.setAttribute("class", linkClass);
-//   divElt.setAttribute("aria-label", stringAL);
-//   console.log(divElt);
-//   divElt.innerHTML = profilPicture;
-// }
-
-// const creatDivMimiKeel = creatDivElt(
-//   "artist-pict",
-//   "Portrait représentant Mimi Keel",
-//   "<img class='artist1' src='../pictures/PhotographersIDPhotos/MimiKeel.jpg' alt='photo de Mimi Keel' />"
-// );
-// console.log(creatDivMimiKeel);
-// const creatDivERWilkens = creatDivElt(
-//   "artist-pict",
-//   "Portrait représentant Elie-Rose Wilkens"
-// );
-// console.log(creatDivERWilkens);
-
-// const tagUlCard = document.createElement("ul");
-// const liElt = document.createElement("li");
-// ulElt.appendChild(liElt);
-// liElt.setAttribute("class", "link-tag");
-// function tagsData(i) {
-//   let linkTags = data.photographers[i].tags;
-//   for (let i = 0; i < linkTags.length; i += 1) {}
-// }
--- js/photographDetail.js
+++ js/photographDetail.js
@@ -1,140 +1,98 @@
-/** page photographe :
- * lorsque l'on clique sur une photo d'artiste ceci ouvre la page photographe de l'artiste avec les bonne image, le bon nom et le bon carroussel
- * changer le H1 page et le h2 form pour qu'il soit en adéquation avec la page de l'artiste
- *
- */
-
-// quand tu click sur data.photographer[0] et que ca ouvre le lien alinkElt on atterit sur une page
-// avec les infos du photograph [0]
-
-// const pHcard = document.getElementsByClassName("photograph__card")[0];
-
-// pHcard.addEventListener("click", openPhPage({}));l
-// let aLinkElt = document.createElement("a");
-
-// if (aLinkElt.data.photographers[i].onclick) {
-// show.data.photographers[i].elt
-// }
+// fetch json file
+fetch("./js/data.json")
+  .then((response) => response.json())
+  .then((phData) => {
+    console.table(phData.photographers);
+    console.table(phData.media);
+  });
 
 // tag main Dom
 const mainPHPage = document.getElementsByTagName("main");
 
 // creat header profil photograph
 function creatHeaderProfil() {
-  const newHeader = document.createElement("header");
-  mainPHPage[0].prepend(newHeader);
-  newHeader.setAttribute("class", "ph-info");
-  newHeader.setAttribute("aria-label", "photographer information");
+  fetch("./js/data.json")
+    .then((response) => response.json())
+    .then((phData) => {
+      const dataForPh = phData.photographers[0];
+      const newHeader = document.createElement("header");
+      mainPHPage[0].prepend(newHeader);
+      newHeader.setAttribute("class", "ph-info");
+      newHeader.setAttribute("aria-label", "photographer information");
+      newHeader.innerHTML = `<section class="ph-card">
+                                  <h1 class="ph-card__artist">${dataForPh.name}</h1>
+                                  <aside class="ph-card__information">
+                                  <p class="ph-card__information--city">${dataForPh.city}/${dataForPh.country}</p>
+                                  <p class="ph-card__information--tagline">${dataForPh.tagline}</p>
+                                  <ul class="tagBox"></ul></aside></section>
+                                  <button type="button" class="ph-contact btn" title="contactez-moi">contactez-moi</button>
+                                  <img src="media/PhotographersIDPhotos/${dataForPh.portrait}"
+                                      alt="Portrait représentant ${dataForPh.portrait}"
+                                        class="profil-picture">`;
 
-  const sectionElt = document.createElement("section");
-  newHeader.appendChild(sectionElt);
-  sectionElt.setAttribute("class", "ph-card");
-
-  const h1Elt = document.createElement("h1");
-  sectionElt.appendChild(h1Elt);
-  h1Elt.setAttribute("class", "ph-card__artist");
-  h1Elt.textContent = data.photographers[0].name;
-
-  const asideElt = document.createElement("aside");
-  h1Elt.appendChild(asideElt);
-  asideElt.setAttribute("class", "ph-card__information");
-
-  const pElt = document.createElement("p");
-  asideElt.appendChild(pElt);
-  pElt.setAttribute("class", "ph-card__information--city");
-  pElt.textContent =
-    data.photographers[0].city + "/" + data.photographers[0].country;
-
-  const p2Elt = document.createElement("p");
-  asideElt.appendChild(p2Elt);
-  p2Elt.setAttribute("class", "ph-card__information--tagline");
-  p2Elt.textContent = data.photographers[0].tagline;
-
-  const ulTag = document.createElement("ul");
-  asideElt.appendChild(ulTag);
-  ulTag.setAttribute("class", "tagBox");
-  const tagsData = data.photographers[0].tags;
-  for (let t = 0; t < tagsData.length; t += 1) {
-    const liTagElt = document.createElement("li");
-    ulTag.appendChild(liTagElt);
-    liTagElt.setAttribute("class", "link-tag");
-    const aTagElt = document.createElement("a");
-    liTagElt.appendChild(aTagElt);
-    aTagElt.setAttribute("href", "#");
-    aTagElt.setAttribute("title", tagsData[t]);
-    aTagElt.textContent = "#" + tagsData[t];
-  }
-
-  const contactButton = document.createElement("button");
-  newHeader.appendChild(contactButton);
-  contactButton.setAttribute("type", "button");
-  contactButton.setAttribute("class", "ph-contact btn");
-  contactButton.setAttribute("title", "contactez-moi");
-  contactButton.textContent = "contactez-moi";
-
-  const pictureElt = document.createElement("img");
-  newHeader.appendChild(pictureElt);
-  pictureElt.setAttribute("class", "profil-picture");
-  pictureElt.setAttribute("src", data.photographers[0].portrait);
-  pictureElt.setAttribute(
-    "alt",
-    "Portrait représentant: " + data.photographers[0].name
-  );
+      const tagsData = phData.photographers[0].tags;
+      for (let t = 0; t < tagsData.length; t += 1) {
+        const ulTagBox = document.getElementsByClassName("tagBox");
+        const liTagElt = document.createElement("li");
+        ulTagBox[0].appendChild(liTagElt);
+        liTagElt.setAttribute("class", "link-tag");
+        liTagElt.innerHTML = `<a href="#" tittle="${tagsData[t]}" >#${tagsData[t]}</a> `;
+      }
+    });
 }
 
-// portofolio
+// create portofolio
 const articlePhPage = document.getElementsByTagName("article");
 
 function creatPictureCard() {
-  let i;
-  for (i = 0; i < data.media.length; i += 1) {
-    const newSection = document.createElement("section");
-    articlePhPage[0].appendChild(newSection);
-    newSection.setAttribute("class", "artist-cards");
-
-    const aLinkLightbox = document.createElement("a");
-    newSection.prepend(aLinkLightbox);
-    aLinkLightbox.setAttribute("href", "#");
-    aLinkLightbox.setAttribute("id", "openLightbox");
-
-    const imgIdElt = document.createElement("img");
-    aLinkLightbox.appendChild(imgIdElt);
-    imgIdElt.setAttribute("src", ""); //inclure le bon chemin d'image
-    imgIdElt.setAttribute("alt", ""); //data.media[i].tags
-    imgIdElt.setAttribute("class", "artist-cards__picture");
-
-    const asideTxtElt = document.createElement("aside");
-    newSection.appendChild(asideTxtElt);
-    asideTxtElt.setAttribute("class", "artist-cards__information");
-
-    const h2TittlePicture = document.createElement("h2");
-    asideTxtElt.appendChild(h2TittlePicture);
-    h2TittlePicture.setAttribute("class", "card-title");
-    h2TittlePicture.textContent = data.media[i].id;
-
-    const pPriceElt = document.createElement("p");
-    asideTxtElt.appendChild(pPriceElt);
-    pPriceElt.setAttribute("class", "card-price");
-    pPriceElt.textContent = data.media[i].price + " €";
-
-    const spanLikeElt = document.createElement("span");
-    pPriceElt.appendChild(spanLikeElt);
-    spanLikeElt.setAttribute("class", "card-likeNumbers");
-    spanLikeElt.textContent = data.media[i].likes;
-
-    const iconeHeartElt = document.createElement("i");
-    pPriceElt.appendChild(iconeHeartElt);
-    iconeHeartElt.setAttribute("class", "fas fa-heart");
-    iconeHeartElt.setAttribute("aria-label", "like");
-    iconeHeartElt.setAttribute("role", "button");
-    iconeHeartElt.setAttribute("tabindex", "0");
-  }
+  fetch("./js/data.json")
+    .then((response) => response.json())
+    .then((phData) => {
+      const dataForMedia = phData.media;
+      let m;
+      for (m = 0; m < dataForMedia.length; m += 1) {
+        const newSection = document.createElement("section");
+        articlePhPage[0].appendChild(newSection);
+        newSection.setAttribute("class", "artist-cards");
+        newSection.innerHTML = `<a href="#" id="openLightbox">
+                                  <img src="media/artistsPictures/${dataForMedia[m].image}"
+                                   id="${dataForMedia[m].id}"
+                                   alt="${dataForMedia[m].alt}"
+                                   tag="${dataForMedia[m].tags}"
+                                   date="${dataForMedia[m].date}"
+                                   class="artist-cards__picture">
+                                  </a>
+                                  <aside class="artist-cards__information">
+                                  <h2 class="card-title">${dataForMedia[m].alt}</h2>
+                                  <p class="card-price">${dataForMedia[m].price}€
+                                  <span class="card-likeNumbers">${dataForMedia[m].likes}</span>
+                                  <i class="fas fa-heart" aria-label="like" role="button" tabindex="0"></i></p></aside>`;
+      }
+      // <embed src="media/artistsVideos/${dataForMedia[m].video}"></embed>
+    });
 }
+
+// dataOfJson.media;
+// console.log(dataOfJson.media);
+
+// function showGoodPict() {
+//   let t;
+//   for (t = 0; t < dataOfJson.photographers.length; t += 1) {
+//     const pHId = dataOfJson.photographers[t].id;
+//     pHId;
+//     console.log(dataOfJson.photographers[t].id);
+//   }
+
+//   let i;
+//   for (i = 0; i < dataOfJson.media.length; i += 1) {
+//     const dataId = dataOfJson.media[i].photographerId;
+//     dataId;
+//     console.log(dataId);
+//   }
+// }
+// console.log(showGoodPict());
 
 window.onload = () => {
   creatHeaderProfil();
   creatPictureCard();
 };
-
-// const photographPage = creatHeaderProfil();
-// photographPage;
